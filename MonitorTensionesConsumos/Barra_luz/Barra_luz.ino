6bdb1a0ede10c6d36ebc8f6d3dfca4b27c2ab4b2
DiverBOT-Educativa <39483235+DiverBOT-Educativa@users.noreply.github.com>
2021-03-16 18:59:57 +0100

    Update Barra_luz.ino

--- MonitorTensionesConsumos/Barra_luz/Barra_luz.ino
+++ MonitorTensionesConsumos/Barra_luz/Barra_luz.ino
@@ -1,88 +1,147 @@
-// Función cambiar colores
-// Función aplique colores
-
+#include <arduino.h>
 #include <Adafruit_NeoPixel.h>
 
+#define VBatPin         A7
+#define LedVBatPin      8
 
-#define LED_PIN 8
-#define LED_COUNT 16
-#define LED_COUNT_LITTLE 8
+#define VServosPin      A6
+#define LedVServosPin   9
+
+#define IDcMotorsPin    A3
+#define LedIDcMotorsPin 10
+
+#define IDcBatPin       A2
+#define LedIDcBatPin    11
+
+#define IServosPin      A1
+#define LedIServosPin   12
+
+#define IJetsonNanoPin  A0
+#define LedIJetsonNanoPin 13
 
 
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+class DisplayBar {
+  public:
+    DisplayBar(byte pinDin, byte pinAn, int valorMin, int valorMax, byte nPixel, unsigned int tPeakPersist);
+    void Begin();
+    void Dibujar();
 
+  private:
+    Adafruit_NeoPixel strip;
+    byte _pinDin;
+    byte _pinAn;
+    int _valorMin;
+    int _valorMax;
+    byte _nPixel;
+    unsigned int _tPeakPersist;
+    unsigned long tiempoPico;
+    int valorAnalogico;
+    int valorPico;
+    int valor;
+    void DibujarPico();
+    int MapValue(int value);
+};
 
-void bar(int endPoint, uint32_t color) {
+DisplayBar::DisplayBar(byte pinDin, byte pinAn, int valorMin, int valorMax, byte nPixel, unsigned int tPeakPersist) {
+  _pinDin = pinDin;
+  _pinAn = pinAn;
+  _valorMin = valorMin;
+  _valorMax = valorMax;
+  _nPixel = nPixel;
+  _tPeakPersist = tPeakPersist;
+  tiempoPico = 0;
+  valorPico = 0;
+}
+void DisplayBar::Begin() {
+  strip = Adafruit_NeoPixel(_nPixel, _pinDin, NEO_GRB + NEO_KHZ800);
+  strip.begin();
   strip.clear();
-  for (int i = 0; i < endPoint; i++) {
-    strip.setPixelColor(i, color);
+}
+int DisplayBar::MapValue(int value) {
+  return map(value, _valorMin, _valorMax, 0, _nPixel);
+}
+void DisplayBar::Dibujar() {
+  valorAnalogico = analogRead(_pinAn);
+  valor = MapValue(valorAnalogico);
+  strip.clear();
+  for (int i = 0; i < valor; i++) {
+    strip.setPixelColor(i, strip.Color(100, 0, 0));
   }
+  DibujarPico();
   strip.show();
 }
+void DisplayBar::DibujarPico() {
+  if (tiempoPico < millis()) {
+    valorPico --;
+    tiempoPico = tiempoPico + 250;
+  }
+  if (valor > valorPico) {
+    valorPico = valor;
+    tiempoPico = millis() + _tPeakPersist;
+  }
+  valorPico = constrain(valorPico, 0, _nPixel - 1);
+  strip.setPixelColor(valorPico, strip.Color(0, 100, 0));
+}
+
+/*Aqui empieza nuestro programa*/
+
+/*DisplayBar BarLeds[] = {
+  DisplayBar(LedVBatPin, VBatPin, 0, 1023, 16, 2000),
+  DisplayBar(LedVServosPin, VServosPin, 0, 1023, 8, 2000)
+};*/
+DisplayBar displayVBat(LedVBatPin, VBatPin, 441, 515, 16, 1000);
+DisplayBar displayVServos(LedVServosPin, VServosPin, 920, 1023, 8, 1000);
+DisplayBar displayIdcMotors(LedIDcMotorsPin, IDcMotorsPin,512,600,8,500);
+DisplayBar displayIDcBat(LedIDcBatPin, IDcBatPin,512,600,8,500);
+//IServosPin
+DisplayBar displayIServos(LedIServosPin, IServosPin,512,600,8,500);
+//IJetsonNanoPin
+DisplayBar displayIJetsonNano(LedIJetsonNanoPin, IJetsonNanoPin,512,600,8,500);
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-}
-
-
-void measureSensor(int analog_pin, int sensor_bar_pin){
-  Adafruit_NeoPixel sensor_bar(LED_COUNT_LITTLE, sensor_bar_pin, NEO_GRB + NEO_KHZ800);
-  int r = map(analogRead(A7), 511, 1023, 0, LED_COUNT_LITTLE);
-  uint32_t color;
+  Serial.begin(9600);
+  Serial.println("Programa iniciado");
+  /*for (int displayBar = 0; displayBar < sizeof(BarLeds); displayBar++) {
+    BarLeds[displayBar].Begin();
+  }*/
+  //BarLeds[0].Begin();
+  displayVBat.Begin();
+  displayVServos.Begin();
+  displayIdcMotors.Begin();
+  displayIDcBat.Begin();
+  displayIServos.Begin();
+  displayIJetsonNano.Begin();
   
-  if (r < 6) {
-    color = strip.Color(255, 0, 0);
-    
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  for(int i = 0; i < 4; i++){
+    digitalWrite(6, LOW);
+    delay(500);
+    digitalWrite(6,HIGH);
+    delay(500);
   }
-  else if (r < 12) {
-    color = strip.Color(100, 100, 0);
-  }
-  else {
-    color =(r, strip.Color(0, 100, 0);
-  }
-
-  strip.clear();
-  for (int i = 0; i < endPoint; i++) {
-    strip.setPixelColor(i, color);
-  }
-  strip.show();
   
 }
-
-/*
- Analog   Input
-  1023 ->  25
-    x  ->  tutension
-
- Max: 
- */
-void batterySensor(){
-  int r = map(analogRead(A7), 441, 500, 0, LED_COUNT);
-  if (r < 6) {
-    bar(r, strip.Color(255, 0, 0));
-    
-  }
-  else if (r < 12) {
-    bar(r, strip.Color(100, 100, 0));
-  }
-  else {
-    bar(r, strip.Color(0, 100, 0));
-  }
-}
-
-/*
- A7 -> Tensión batería
- A6 -> Tensión servos
- A3 -> Corriente motores DC
- A2 -> Corriente general/batería
- A1 -> Consumo servos
- A0 -> Jetson Nano
-*/
 
 void loop() {
-  batterySensor();
-  measureSensor(A0, 9);
-
+  /*for (int displayBar = 0; displayBar < sizeof(BarLeds); displayBar++) {
+    BarLeds[displayBar].Dibujar();
+    Serial.println("Dibujando");
+  }*/
+  displayVBat.Dibujar();
+  displayVServos.Dibujar();
+  displayIdcMotors.Dibujar();
+  displayIDcBat.Dibujar();
+  displayIServos.Dibujar();
+  displayIJetsonNano.Dibujar();
+  Serial.println(analogRead(IDcMotorsPin));
 }
+
+/* Como guardar un color en un uint32
+
+  uint32_t Adafruit_NeoPixel::Color(uint8_t r, uint8_t g, uint8_t b) {
+  return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+  }
+
+*/
