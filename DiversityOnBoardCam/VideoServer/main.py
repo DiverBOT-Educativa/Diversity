3514f8f1fbd8c252c26114525a72419a7c1096c2
Josep Mengual <josepmengual@protonmail.com>
2021-03-24 23:38:07 +0100

    Limpieza y organización de código

--- DiversityOnBoardCam/VideoServer/main.py
+++ DiversityOnBoardCam/VideoServer/main.py
@@ -1,4 +1,8 @@
-import socket, struct, pickle, threading, cv2
+import socket
+import struct
+import pickle
+import threading
+import cv2
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,17 +11,23 @@ server_socket.listen()
 
 image = cv2.imread("tux.webp")
 
+
 def accept_client(client_socket, addr):
-    result, frame = cv2.imencode('.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+    # Convertir imagen a jpeg con calidad 90%
+    result, frame = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
     packed_image = pickle.dumps(frame)
     img_size = struct.pack("Q", len(packed_image))
     packet = img_size + packed_image
     client_socket.send(packet)
 
+
 while True:
     try:
         client_socket, addr = server_socket.accept()
-        client_thread = threading.Thread(target=accept_client, args=(client_socket, addr))
+        client_thread = threading.Thread(
+            target=accept_client, args=(client_socket, addr)
+        )
         client_thread.start()
     except KeyboardInterrupt:
         break
