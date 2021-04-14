4366c64732b12f40e4a0794d66dcd4f2ed3c5aac
DiverBOT-Educativa <39483235+DiverBOT-Educativa@users.noreply.github.com>
2021-04-14 11:53:14 +0200

    Create servoController.py
    Clase creada para controlar los servos. Falta añadir los servos de la cámara.

---
+++ DiversityControlServos/servoController.py
@@ -0,0 +1,64 @@
+from time import sleep
+import Adafruit_PCA9685
+
+VelocidadGiro = 0.05
+PosicionServoAD = 0
+PosicionIr = 180
+
+class ServoController():
+
+	def __init__(self, busnum=1):
+		self.pwm = Adafruit_PCA9685.PCA9685(busnum=busnum)
+		self.pwm.set_pwm_freq(60)
+
+		self.minValues = [100, 140, 125, 125]
+		self.maxValues = [575, 610, 590, 590]
+
+	def setAngle(self, servo, angle):
+		if (angle < 0): angle = 0
+		if (angle > 180): angle = 180
+		minValue = self.minValues[servo]
+		maxValue = self.maxValues[servo]
+
+		pulse = (angle - 0) * (maxValue - minValue) / (180 - 0) + minValue
+		self.pwm.set_pwm(servo, 0, int(pulse))
+
+if (__name__ == "__main__"):
+	sc = ServoController(1)
+	sc.setAngle(1, PosicionServoAD)
+
+
+	while True:
+		sc.setAngle(1, PosicionServoAD + 1)
+		PosicionServoAD = PosicionServoAD + 1
+		sleep(VelocidadGiro)
+		if PosicionServoAD >= PosicionIr:
+			break
+
+	sc.setAngle(1, 0)
+	sc.setAngle(1, 90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
